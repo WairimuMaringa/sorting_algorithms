c0d8a8f2e4f0781ede714e4e3498cf4834b5307c
WairimuMaringa <makena96wairimu@gmail.com>
2022-10-11 12:08:03 -0700

    advanced task one

--- sort.h
+++ sort.h
@@ -27,6 +27,8 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void _qsort(int *array, int head, int tail, size_t size);
+void shell_sort(int *array, size_t size);
+void _ssort(int *arr, int size, int n);
 
 
 #endif
